1d576937091697c60dfc2081d551f45bf8033223
yadav-sachin <sachinyadav7024@gmail.com>
2021-02-07 20:40:17 +0530

    static

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/editReview")
+def edit():
+    return render_template("edit_review.html")
+
+@app.route("/addReview")
+def add():
+    return render_template("add_review.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
